a92323c32aa86a6f7eb1a980c0400b805c720b55
Katherine Rosenfeld <56368533+krosenfeld-IDM@users.noreply.github.com>
2025-05-02 00:09:00 +0000

    install codex

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -4,3 +4,8 @@ uv tool install tox --with tox-uv > /tmp/tox.stdout.log 2> /tmp/tox.stderr.log
 uv venv --python 3.10 > /tmp/venv.stdout.log 2> /tmp/venv.stderr.log
 source .venv/bin/activate
 uv pip install -e .[nb] > /tmp/install.stdout.log 2> /tmp/install.stderr.log
+
+# Install Node.js and npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+nvm install node
+npm i -g @openai/codex
